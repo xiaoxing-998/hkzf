02ef58a6f7171df9706bbc6e75eb191d572fa15f
unknown <sstar>
2020-03-09 21:18:39 +0800

    全局导航-底部Tabbar-基本配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // 根组件
 import React from 'react';
 // 路由三个基础组件
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // 导入路由对应组件
 import Home from './pages/Home';
@@ -12,9 +12,6 @@ import NotFound from './pages/NotFound';
 function App() {
     return (
         <Router className="app">
-            <Link to='/home'>首页</Link>
-            <Link to='cityList'>城市列表</Link>
-            <Link to='/map'>地图</Link>
             <Switch>
                 {/* 一级路由 */}
                 <Route path='/home' component={Home} />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 // 路由基础组件
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+// 引入Ant Design组件库组件
+import { TabBar } from 'antd-mobile';
 
 // 导入二级路由对应组件
 import Index from '../Index';
@@ -8,22 +10,116 @@ import House from '../House';
 import Profile from '../Profile';
 
 class Home extends Component {
+
+    // tab状态数据
+    state = {
+        selectedTab: 'redTab',//选中状态
+        fullScreen: true//是否满屏
+    }
+
     render() {
         return (
             <div className='home'>
                 {/* 配置二级路由 */}
-                <div>
-                    <Link to='/home'>默认首页</Link>
-                    <Link to='/home/house'>找房</Link>
-                    <Link to='/home/profile' >我的</Link>
-                </div>
                 <Route exact path='/home' component={Index} />
                 <Route path='/home/house' component={House} />
                 <Route path='/home/profile' component={Profile} />
+                {/* 全局导航 */}
+                <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+                    <TabBar
+                        unselectedTintColor="#949494"
+                        tintColor="#33A3F4"
+                        barTintColor="white"
+                    >
+                        <TabBar.Item
+                            title="首页"
+                            key="Life"
+                            icon={<div style={{
+                                width: '22px',
+                                height: '22px',
+                                background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat'
+                            }}
+                            />
+                            }
+                            selectedIcon={<div style={{
+                                width: '22px',
+                                height: '22px',
+                                background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat'
+                            }}
+                            />
+                            }
+                            // 是否选中 返回布尔值
+                            selected={this.state.selectedTab === 'blueTab'}
+                            // bar 点击触发函数   设置点击状态
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'blueTab',
+                                });
+                            }}
+                        >
+                        </TabBar.Item>
+                        <TabBar.Item
+                            icon={
+                                <div style={{
+                                    width: '22px',
+                                    height: '22px',
+                                    background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+                                }}
+                                />
+                            }
+                            selectedIcon={
+                                <div style={{
+                                    width: '22px',
+                                    height: '22px',
+                                    background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+                                }}
+                                />
+                            }
+                            title="找房"
+                            key="Koubei"
+                            selected={this.state.selectedTab === 'redTab'}
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'redTab',
+                                });
+                            }}
+                        >
+                        </TabBar.Item>
+                        <TabBar.Item
+                            icon={
+                                <div style={{
+                                    width: '22px',
+                                    height: '22px',
+                                    background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+                                }}
+                                />
+                            }
+                            selectedIcon={
+                                <div style={{
+                                    width: '22px',
+                                    height: '22px',
+                                    background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+                                }}
+                                />
+                            }
+                            title="我的"
+                            key="Friend"
+                            selected={this.state.selectedTab === 'greenTab'}
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'greenTab',
+                                });
+                            }}
+                        >
+                        </TabBar.Item>
+                    </TabBar>
+                </div>
+
 
             </div>
         );
     }
 }
+
 
 export default Home;
